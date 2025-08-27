feb916abdd890bd30a2ad2aa6823895d93d713e2
NgoHuy05 <160807769+NgoHuy05@users.noreply.github.com>
2025-08-27 11:37:08 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <>
             <div className="bg-gradient-to-b from-amber-200 to-gray-100 border-t-2 border-dashed flex flex-col gap-2 items-center mt-20 p-10">
                 <div className="text-2xl font-bold border-b-2 border-amber-600 p-2">Portfolio</div>
-                <div>© 2025 Huy - PhaseFirst.</div>
+                <div>© 2025 Huy - Level 1.</div>
 
             </div>
         </>
